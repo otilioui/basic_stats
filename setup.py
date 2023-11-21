7af288a49a53b05b765734c2c8d28590da4b4a31
otilioui <oliver.tiliouine@uri.edu>
2023-11-21 18:11:19 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "air_sea",
+    version = "0.0.1",
+    author = "Brice Loose",
+    packages=['sat_vp'],
+    install_requires=['numpy','scipy.io'],
+)
+
