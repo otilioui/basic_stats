2b2513cbc1cc2a5136b19a1f62c7fbb8c9d75b38
otilioui <oliver.tiliouine@uri.edu>
2023-11-21 18:34:48 -0500

    basic stats

---
+++ basic_stats.py/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Air-sea Package for Python
+    Version 1.0 Nov. 21, 2023
+    Author:  Oliver Tiliouine, Department of Marine Affairs, URI.
+    Email: oliver.tiliouine@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .air_sea import *
+
+
+__all__ = ['sat_vp']
